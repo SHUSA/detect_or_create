b1a4348a2925dd811d6e84b1e1782087f26191a4
jnwheeler44 <jnwheeler44@gmail.com>
2014-10-09 09:31:17 -0700

    Adds Yard comments

--- lib/detect_or_create/class_methods.rb
+++ lib/detect_or_create/class_methods.rb
@@ -1,5 +1,13 @@
 module DetectOrCreate
   module ClassMethods
+
+    # Finds or creates an object, based on parameters provided
+    #
+    # @param args [Hash] Parameters to detect or create
+    # @option args [Hash] :by The parameters to use when finding a record
+    # @option args [Hash] :with The parameters to use to update or create the record
+    # @option args [Boolean] :overwrite_existing Allow overwriting of existing records
+    # @return [Object] the updated instantiated or created object
     def detect_or_create(args={})
       if errors = invalid_arguments(args) and errors.present?
         raise StandardError, "#{errors}"
@@ -16,25 +24,44 @@ module DetectOrCreate
 
     private
 
+    # Sets required keys for argument hash
+    #
+    # @return [Array] Required keys to be sent in the options hash.
     def required_keys
       [:by]
     end
 
+    # Only allow certain object types with specific keys
+    #
+    # @return [Hash] Used to set what type of object is allowed with certain parameters
     def required_types
       { :by => Hash,
         :with => Hash }
     end
 
+    # Verify existence of required keys
+    #
+    # @param args [Hash] Argument hash
+    # @return [Array] Required keys that have not been provided as arguments
     def missing_keys(args)
       required_keys - args.keys
     end
 
+    # Verify argument object types match with required_types
+    #
+    # @param args [Hash] Argument Hash
+    # @return [String] Error strings to return to consumer for mismatched key types
     def mismatched_types(args)
       required_types.collect { |key, type|
         "`#{key}` must be a #{type}" if args[key].present? && !args[key].is_a?(type)
       }.compact.join("\n")
     end
 
+
+    # Validates arguments hash
+    #
+    # @param args [Hash] Argument Hash
+    # @return [String] If invalid, error string is returned
     def invalid_arguments(args)
       case
       when missing_keys(args).present?
